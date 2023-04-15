f26a1cc7d138d4b1de0b7d8e6a5ac77e53bc17d2
tjhon <fr.jhonk@gmail.com>
2023-04-14 17:28:48 -0700

    alg

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='NOMBRE_DEL_PAQUETE',
+    name='algo',
     version='0.1',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     entry_points={
         'console_scripts': [
-            'NOMBRE_DEL_PAQUETE=NOMBRE_DEL_PAQUETE.main:main'
+            'algo=algo.main:main'
         ]
     },
     install_requires=[
