983fce4f82179dfa370db892f606e7bf8eafc4ac
tjhon <fr.jhonk@gmail.com>
2023-04-14 17:44:05 -0700

    sa

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='algo',
-    version='0.24',
+    version='0.25',
     packages=["src"],
     install_requires=[
         # any dependencies required by your package
     ],
-	package_dir={"": "src"},
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
